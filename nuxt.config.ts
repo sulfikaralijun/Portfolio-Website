c7fc71976eb077f3cbb9c5316be08d031db4b494
sulfikaralijun <sulfikaralijun@gmail.com>
2024-07-16 14:06:27 +0800

    Revert "feat (subdomain): configure subdomain"
    This reverts commit fb86a5aabbbecac5d35ac398b5d81ba2004474d8.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-
-  nitro: {
-    preset: 'vercel'
-  },
-
-  router: {
-    middleware: ['subdomain']
-  },
-
-  runtimeConfig: {
-    public: {
-      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://sulfikaralijun.my.id',
-    }
-  }
 })
